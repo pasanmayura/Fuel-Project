48f50a0322f5a2c5b98a82cc9edcffacb7d31205
pasanmayura <karunat-im21020@stu.kln.ac.lk>
2025-06-07 22:47:27 +0530

    feat(frontend): implement vehicle registration routing and component integration

--- Frontend/vehicle-owner-frontend/src/app/app.component.ts
+++ Frontend/vehicle-owner-frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RegisterVehicleComponent } from './pages/register-vehicle/register-vehicle.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RegisterVehicleComponent], // Import RegisterVehicleComponent here
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'vehicle-owner-frontend';
