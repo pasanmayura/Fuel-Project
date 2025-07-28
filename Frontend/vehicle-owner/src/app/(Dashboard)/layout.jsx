57782816e5d26cf1b116a611f3ebdd775a1b3c2f
pasanmayura <karunat-im21020@stu.kln.ac.lk>
2025-07-28 18:03:28 +0530

    feat(sidebar): enhance mobile menu functionality and improve sidebar layout

--- Frontend/vehicle-owner/src/app/(Dashboard)/layout.jsx
+++ Frontend/vehicle-owner/src/app/(Dashboard)/layout.jsx
@@ -7,11 +7,11 @@ export default function DashboardLayout({
   return (
     <div className="h-screen flex">
       {/* LEFT */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[15%] p-3">
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[18%] p-3">
         <Sidebar />
       </div>
       {/* RIGHT */}
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[82%] bg-[#F7F8FA] overflow-scroll flex flex-col">
         <Topbar />
         {children}
       </div>
