2d5bcd216cdcb444ff4778481a5bbabbcee653c4
pasanmayura <karunat-im21020@stu.kln.ac.lk>
2025-07-31 04:49:53 +0530

    feat(service): enable JPA repositories for the station service application

--- station-service/src/main/java/com/smartfuel/station_service/StationServiceApplication.java
+++ station-service/src/main/java/com/smartfuel/station_service/StationServiceApplication.java
@@ -2,8 +2,10 @@ package com.smartfuel.station_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.smartfuel.station_service.repository")
 public class StationServiceApplication {
 
 	public static void main(String[] args) {
