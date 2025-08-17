01f10ad42e98499dc37f5c948aae562c3dd5a954
pasanmayura <karunat-im21020@stu.kln.ac.lk>
2025-08-17 18:55:38 +0530

    fix(account): adjust formatting of Account class fields for consistency

--- station-service/src/main/java/com/smartfuel/station_service/model/Account.java
+++ station-service/src/main/java/com/smartfuel/station_service/model/Account.java
@@ -12,12 +12,8 @@ public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String username;
-
-    private String password; 
-
+    private String password;
     private String email;
-
     private String role;
 }
